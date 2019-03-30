f761d089377e4f2e34ff5375b36eec23e9d5b0e7
Amber Koval <amberkoval92@gmail.com>
2019-03-30 16:16:30 -0700

    workingish gifs

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,32 +1,97 @@
 
-        $(document).ready(function () {
+$(document).ready(function () {
 
-            //**DONE** Create for each loop that creates a new button for each item in the array when the page is loaded
-
-            var topics = ["laugh", "cry"];
-
-            for (var i = 0; i < topics.length; i++) {
-
-                let a = $("<button>");
-
-                a.text(topics[i])
-
-                $("#topicButton").append(a);
-            }
+    //**DONE** Create for each loop that creates a new button for each item in the array when the page is loaded
 
 
 
-            for (var i = 0; i < topics.length; i++) {               //**trying to add data-person to each new button
-                $("<button>").attr("data-person", topics)
-            }                                                            //**
+    var topics = ["Laugh", "Cry", "Jump", "Sit", "Cat", "Dog", "Fun", "Boring", "Rain", "Sun", "Left", "Right", "Big", "Small", "Yum", "Eww", "Disney", "DreamWorks"];
+
+    function seeGifs(){
+        var gifItems = $(this).attr("data-name")
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+
+        gifItems + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        })
+
+        .then(function(response){
+            console.log(response)
+
+            $("showGif").empty()
+
+            var responseArray = response.data
+
+            responseArray.forEach(function(responseItem){
+                console.log(responseItem.images.fixed_height.url)
+
+                var newImageDiv = $("<div>")
+
+                var myImage = $("<img>")
+
+                myImage.attr("src", responseItem.images.fixed_height.url)
+                myImage.addClass("myGif")
+
+                newImageDiv.append(myImage)
+
+                var ratingPlace = $("<div>")
+                ratingPlace.text(responseItem.rating)
+
+                newImageDiv.append(ratingPlace)
+
+                $("#showGif").append(newImageDiv)
 
 
 
-        });                                                                                     //end doc
+            })
+
+        })
+
+    }
+
+
+    function displayButtons() {
+
+        $("#showButton").empty();
+
+
+        topics.forEach(function (topicsItems) {
+            var newButton = $("<button>")
+                    newButton.addClass("oppositeGif")
+                        newButton.attr("data-name", topicsItems)
+                            newButton.text(topicsItems)
+
+            $("#showButton").append(newButton)
+
+        })
+    }
+
+    $(".findGif").on('click', function(event){
+
+        event.preventDefault()
+
+        var userChoice = $("#userChoice").val().trim()
+      
+        topics.push(userChoice)
+
+        displayButtons()
+
+
+    })
+
+    displayButtons()
+
+    $(document).on('click', ".oppositeGif", seeGifs)
 
 
 
 
 
 
+
+})                                                                               
 
